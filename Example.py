7b455b2d44d2e6646477985d31ba30017430327b
jacobhbloom1 <58534763+jacobhbloom1@users.noreply.github.com>
2019-12-04 21:57:04 -0800

    Update Example.py

--- Example.py
+++ Example.py
@@ -6,7 +6,7 @@ window.title("GUI")
 # creates canvas
 canvas = tkinter.Canvas(window, width = 500, height = 500)
 canvas.pack()
-
+#create hexagon
 newHex = hexagon()
 newHex.center_coordinates(250,250)
 newHex.hex_side_length(50)
@@ -14,5 +14,12 @@ newHex.hex_color("green")
 newHex.hex_outline("blue")
 newHex.hex_width(20)
 newHex.create_hex()
+#create snowflake
+mySnowflake = snowflake()
+mySnowflake.centerXY(250,250)
+mySnowflake.length(100)
+mySnowflake.create_snowflake()
+
+
 
 window.mainloop()
